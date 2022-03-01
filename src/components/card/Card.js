2c30b93367558b265db04b393670ab069175f2c8
nrj <admin_nrj@mail.ru>
2022-03-01 09:15:57 +0300

    initial commit

---
+++ src/components/card/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './card.module.css'
+
+const Card = () => {
+    return (
+        <div className={s.cardWrapper}>
+            <div className={s.card}>
+                test
+            </div>
+        </div>
+    );
+};
+
+export default Card;
